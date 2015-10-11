6350aeaed03509432f26e1cbcc6c472887832447
mainitiamai <mainitiamai@gmail.com>
2015-10-11 22:39:51 +0900

    プロジェクト追加

---
+++ Assets/MoveLight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLight : MonoBehaviour {
+
+	Transform myTransform;
+	Vector3 m_v3Position;
+
+	public Vector3 m_v3Speed;
+
+	// Use this for initialization
+	void Start () {
+
+		m_v3Speed = new Vector3 (0.0075f, 0.0f, 0.001f);
+
+		myTransform = transform;
+		m_v3Position = myTransform.localPosition;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		//m_v3Position += m_v3Speed;
+		myTransform.localPosition += m_v3Speed;
+
+	}
+}
